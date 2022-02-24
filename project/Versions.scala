5c4a836a91325dcb800aec15b88144e0d4564730
scala.steward <scala-steward@avast.com>
2022-02-24 16:03:36 +0000

    Update micrometer-core, ... from 1.6.5 to 1.6.13

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
 
   val avastMetrics = "2.8.5"
   val catsEffect = "2.4.1"
-  val micrometer = "1.6.5"
+  val micrometer = "1.6.13"
   val scalatest = "3.2.7"
   val slf4j = "1.7.30"
 
