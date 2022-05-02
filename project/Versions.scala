70b21d12148eb75b4865021e8f872a525917c3f6
scala.steward <scala-steward@avast.com>
2022-05-02 00:29:03 +0000

    Update cats-effect from 2.5.4 to 3.3.11

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
 
   val avastMetrics = "2.8.5"
-  val catsEffect = "2.5.4"
+  val catsEffect = "3.3.11"
   val micrometer = "1.6.5"
   val scalatest = "3.2.7"
   val slf4j = "1.7.30"
