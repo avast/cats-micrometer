ecdbaaf5200c8f8e7d6f6da86cfee28ccdbddd0e
Jenda Kolena <jan.kolena@avast.com>
2022-06-23 15:17:21 +0200

    asMetricsCatsEffectMonitor

--- avast-metrics/src/main/scala/com/avast/micrometer/avastMetrics/avastMetrics.scala
+++ avast-metrics/src/main/scala/com/avast/micrometer/avastMetrics/avastMetrics.scala
@@ -1,12 +1,15 @@
 package com.avast.micrometer
 
+import cats.effect.Sync
 import com.avast.metrics.api.Monitor
 import com.avast.metrics.scalaapi.{Monitor => ScalaMonitor}
+import com.avast.metrics.scalaeffectapi.{Monitor => CatsEffectMonitor}
 import com.avast.micrometer.api.CatsMeterRegistry
 
 package object avastMetrics {
   implicit class CatsMeterRegistryExtension[F[_]](private val cmr: CatsMeterRegistry[F]) extends AnyVal {
     def asMetricsMonitor: Monitor = new MicrometerToMonitorAdapter(cmr.underlying)
     def asMetricsScalaMonitor: ScalaMonitor = ScalaMonitor(asMetricsMonitor)
+    def asMetricsCatsEffectMonitor(implicit s: Sync[F]): CatsEffectMonitor[F] = CatsEffectMonitor.wrapJava(asMetricsMonitor)
   }
 }
--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
 
-  val avastMetrics = "2.8.6"
+  val avastMetrics = "2.9.3"
   val catsEffect = "2.5.4"
   val micrometer = "1.6.5"
   val scalatest = "3.2.7"
