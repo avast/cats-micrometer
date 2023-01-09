2259a1744c4fb8f6ec6ad58836a86000b3d4849d
scala.steward <scala-steward@avast.com>
2023-01-09 09:22:21 +0000

    Update scalatest from 3.2.7 to 3.2.15

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val avastMetrics = "2.9.3"
   val catsEffect = "2.5.4"
   val micrometer = "1.6.5"
-  val scalatest = "3.2.7"
+  val scalatest = "3.2.15"
   val slf4j = "1.7.36"
 
 }
